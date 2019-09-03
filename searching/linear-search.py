da618d16d70beaf69f2b872eac0665a274ee0db6
aadidubey7 <aadidubey7@gmail.com>
2019-09-03 14:55:20 +0530

    Traverse through the list

--- searching/linear-search.py
+++ searching/linear-search.py
@@ -1,7 +1,14 @@
-def linearSearch(list(userDefinedList)):
-    position = 0
-    print( userDefinedList )
-
-
-linearSearch( [1, 2, 3, 4, 5] )
+def linearSearch(intList, intItem):
     
+    boolIsFound = False
+    for item in intList:
+    	if item == intItem:
+    		boolIsFound = True
+    		break
+
+    if True == boolIsFound:
+    	print('Item is found at index', intList.index(item))
+    else:
+    	print('Item is not found.')
+
+linearSearch( [1, 2, 3, 4, 5], 4 )
